a9918801c37d0166b6f54534cd5ba539dcb37502
RaphaelSIO <163443546+RaphaelSIO@users.noreply.github.com>
2024-03-28 16:36:12 +0100

    Update connexion.php

--- script/connexion.php
+++ script/connexion.php
@@ -1,1 +1,7 @@
+ if (isset($_GET["login"]) && isset($_GET["mdp"])) {
+        // Récupérer les valeurs soumises dans les champs login et mdp
+        $login = $_GET["login"];     // Vérifier si l'identifiant est correcte
+        $mdp = $_GET["mdp"];         // Vérifier si le mot de passe est correcte 
 
+        
+       
