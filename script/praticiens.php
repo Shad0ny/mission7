31b7aa29c76e785244875d3a8eef813cecd81baf
Shad0ny <151963104+Shad0ny@users.noreply.github.com>
2024-04-04 17:42:58 +0200

    Update and rename praticiens.html to praticiens.php

---
+++ script/praticiens.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<title>Liste des praticiens</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" href="../style/connexion.css">
+<link rel="shortcut icon" href="./img/gsb.png"/>
+</head>
+<body>
+<!-- debut de l'enveloppe globale du site -->
+	<div id="haut">
+	<!-- debut en tete -->
+	<div id="header">
+	<?php
+		include("includes/hautPage.html")
+	?>
+	<!-- fin en tete -->
+	<!-- corps (partie droite) -->
+	<div id="droitecorps">
+</body>
