804a7250196abf4ce374eadaa4d414a4c9cca285
Shad0ny <151963104+Shad0ny@users.noreply.github.com>
2024-04-04 17:44:14 +0200

    Update and rename consulterRapport.html to consulterRapport.php

---
+++ script/consulterRapport.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<title>Accueil</title>
+<link rel="stylesheet" href="../style/connexion.css">
+</head>
+<body>
+<!-- debut de l'enveloppe globale du site -->
+	<div id="haut">
+	<!-- debut en tete -->
+	<div id="header">
+	<?php
+		include("includes/hautPage.html")
+	?>
+	<!-- fin en tete -->
+	<!-- corps (partie droite) -->
+	<div id="droitecorps">
+</body>
+</html>
